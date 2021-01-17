a114b09d007bd94e848943a6b97e25ea83b53906
Eric Arnhem <arnhemeric60@gmail.com>
2021-01-17 17:24:49 +0100

    Added a script to animates the progress bars.

---
+++ assets/js/progressBars.js
@@ -0,0 +1,42 @@
+// Function to load a progress bar
+function loadProgressBar(skillId) {
+
+  var progressBar = document.querySelector(skillId + ' .progress-bar');
+  // Sets the width of the progress bar to 0% to cancel the width given by the CSS
+  progressBar.style.width = 0 + '%';
+  // Width at which the bar begins to load
+  var width = 0;
+  var id = setInterval(frame, 20);
+  // Gets the percentage from the HTML element
+  var percentage = progressBar.innerHTML.replace('%', '');
+
+  function frame() {
+    if (width >= percentage) {
+      clearInterval(id);
+    } else {
+      width++
+      progressBar.style.width = width + '%';
+      progressBar.innerHTML = width + '%';
+    }
+  }
+
+}
+
+// Loading all progress bars
+loadProgressBar('#skill-html');
+loadProgressBar('#skill-css');
+loadProgressBar('#skill-bootstrap');
+loadProgressBar('#skill-js');
+loadProgressBar('#skill-jquery');
+loadProgressBar('#skill-php');
+loadProgressBar('#skill-sql');
+loadProgressBar('#skill-windows');
+loadProgressBar('#skill-linux');
+loadProgressBar('#skill-vscode');
+loadProgressBar('#skill-nano');
+loadProgressBar('#skill-commandline');
+loadProgressBar('#skill-git');
+loadProgressBar('#skill-github');
+loadProgressBar('#skill-wamp');
+loadProgressBar('#skill-paintnet');
+loadProgressBar('#skill-pencil');
