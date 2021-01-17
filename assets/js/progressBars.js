17f9b2ddcb926a214db93b40c8203cc5ac9eb127
Eric Arnhem <arnhemeric60@gmail.com>
2021-01-17 20:04:09 +0100

    Added some comments on the progressBars script.

--- assets/js/progressBars.js
+++ assets/js/progressBars.js
@@ -1,19 +1,23 @@
 // Function to load a progress bar
 function loadProgressBar(skillId) {
 
+  // Selects the progress bar wanted
   var progressBar = document.querySelector(skillId + ' .progress-bar');
   // Sets the width of the progress bar to 0% to cancel the width given by the CSS
   progressBar.style.width = 0 + '%';
   // Width at which the bar begins to load
   var width = 0;
+  // Calls the frame function every 20ms until it's cleared
   var id = setInterval(frame, 20);
   // Gets the percentage from the HTML element
   var percentage = progressBar.innerHTML.replace('%', '');
 
   function frame() {
+    // If the progress bar has finished loading, we clear the interval
     if (width >= percentage) {
       clearInterval(id);
     } else {
+      // If not, we keep loading it
       width++
       progressBar.style.width = width + '%';
       progressBar.innerHTML = width + '%';
